490a17f8c3e7f13cad7ca0bd0c52ce8be89748d5
Bernardo <bernardo.bt@gmail.com>
2021-06-28 10:54:16 -0300

    style: adição de classe

--- src/Components/Meals/MealItem/MealsItem.jsx
+++ src/Components/Meals/MealItem/MealsItem.jsx
@@ -5,7 +5,7 @@ const MealsItem = (props) => {
     const price = `$${props.price.toFixed(2)}`
 
     return (
-        <li className={classes.meal}>
+        <li  className={classes.meal}>
             <div>
                 <h3>{props.name}</h3>
                 <div className={classes.description}>{props.description}</div>
