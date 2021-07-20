16bc81a6cf935507b5af06fb6f0a1a12f380e28d
Bernardo <bernardo.bt@gmail.com>
2021-07-20 08:40:51 -0300

    feat: implementção funcionalidade de adição de itens no carrinho - variavel de contexto e lift he state up

--- src/Components/Meals/MealItem/MealItemForm.jsx
+++ src/Components/Meals/MealItem/MealItemForm.jsx
@@ -1,19 +1,45 @@
+import { useRef, useState } from 'react'
 import classes from './MealItemForm.module.css'
 import Input from '../../UI/Input'
 
 const MealItemForm = (props) =>{
+
+    const [amountIsValid, setAmountIsValid] = useState(true)
+    const amountInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+        console.log(enteredAmountNumber)
+
+
+        if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            setAmountIsValid(false)
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber)
+    }
+
     return(
-        <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: 'amount' + props.id,
-                type: 'number',
-                min: '1',
-                max:'5',
-                step:'1',
-                value:'1',
-                className: 'sasdasdas'
-            }} />
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input 
+                label="Amount" 
+                ref={amountInputRef}
+                input={{
+                    id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max:'5',
+                    step:'1',
+                    defaultValue:'1',
+                    
+                }}                 
+            />
             <button>+ add</button>
+            {!amountIsValid  && <p> please enter a valid amount (1-5)</p>}
         </form>
     )
 }
--- src/Components/Meals/MealItem/MealsItem.jsx
+++ src/Components/Meals/MealItem/MealsItem.jsx
@@ -1,8 +1,25 @@
+import { useContext } from 'react'
+import CartContext from '../../../store/cart-context';
+
 import classes from './MealsItem.module.css'
 import MealItemForm from './MealItemForm'
 
 const MealsItem = (props) => {
-    const price = `$${props.price.toFixed(2)}`
+    
+    const cartCtx = useContext(CartContext);
+    const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+
+        console.log(`amount: ${amount}`)
+
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
 
     return (
         <li  className={classes.meal}>
@@ -12,7 +29,7 @@ const MealsItem = (props) => {
                 <div className={classes.price}>{price}</div>    
             </div>
             <div>
-                <MealItemForm id={props.id} />
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     )
