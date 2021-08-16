8497b51fbda6c2fadfe510905ab5bfde804a6ac6
Bernardo <bernardo.bt@gmail.com>
2021-08-16 12:06:18 -0300

    feature: consulta a lista de meals usando o firebase e fetch API

--- src/Components/Meals/AvaliableMeals.jsx
+++ src/Components/Meals/AvaliableMeals.jsx
@@ -1,54 +1,62 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MealsItem from './MealItem/MealsItem';
 import Card from '../UI/Card'
 import classes from './AvailableMeals.module.css'
+import firebase_url from '../../firebase_url';
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map( meal => {
-        return(            
-          <MealsItem 
-              id={meal.id}                     
-              name={meal.name} 
-              description={meal.description} 
-              price={meal.price}
-              key={meal.id}
-          />             
-        )
-    })
+   
+   const [meals, setMeals] = useState([]);
+   const [isloading, setIsLoading] = useState(false)
+
+   useEffect(() => {
+      const fetchMeals = async () => {
+
+         setIsLoading(true)
+         
+         const response = await fetch(firebase_url);
+         const responseData = await response.json();
+
+         const loadedMeals = [];
+
+         for (const key in responseData) {
+               loadedMeals.push({
+               id: key,
+               name: responseData[key].name,
+               description: responseData[key].description,
+               price: responseData[key].price,
+            });
+         }
+
+         setMeals(loadedMeals);
+         setIsLoading(false)
+      };
+  
+      fetchMeals();
+    }, []);
+
+   const mealsList = meals.map( meal => {
+      return(            
+         <MealsItem 
+            id={meal.id}                     
+            name={meal.name} 
+            description={meal.description} 
+            price={meal.price}
+            key={meal.id}
+         />             
+      )
+   })
 
     return(
         <section className={classes.meals}>
             <Card>
-                <ul>
-                    {mealsList}
-                </ul>
+               {  
+                  isloading 
+                     ? <p> loading...</p>
+                     : <ul> {mealsList} </ul>
+               }
+               
             </Card>
         </section>
     )
