fdecfd8be96db0b2bd6a8552cd6849e06ab5de5d
Bernardo <bernardo.bt@gmail.com>
2021-08-17 15:05:21 -0300

    feature: validação de inputs e do formulário de checkout

--- src/Components/Cart/Checkout.jsx
+++ src/Components/Cart/Checkout.jsx
@@ -1,28 +1,90 @@
+import { useRef, useState } from 'react';
 import classes  from './Checkout.module.css';
+
+const isEmpty = value => value.trim() === '';
+const isFiveChars = value => value.trim().length === 5;
 
 const Checkout = props => {
 
+    const [inputsIsValid, setInputsIsValid] = useState({
+        name: true,
+        street: true,
+        postalCode: true,
+        city: true,
+    })
+
+    const userNameInputRef = useRef('');
+    const streetInputRef = useRef('');
+    const postalCodeInputRef = useRef('');
+    const cityInputRef = useRef('');
+
+   
+
     const confirmHandler = (event) => {
+        
         event.preventDefault()
+
+        const enteredName = userNameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredPostalCode = postalCodeInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
+
+        console.log(userNameInputRef.current.value);
+
+        const enteredNameIsValid = !isEmpty(enteredName);
+        const enteredStreetIsValid = !isEmpty(enteredStreet);
+        const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+        const enteredCityIsValid = !isEmpty(enteredCity);
+
+        const formIsValid = enteredNameIsValid && enteredStreetIsValid && enteredPostalCodeIsValid && enteredCityIsValid;
+
+        if (!formIsValid){
+            return (
+                setInputsIsValid({
+                    name: enteredNameIsValid,
+                    street: enteredStreetIsValid,
+                    postalCode: enteredPostalCodeIsValid,
+                    city: enteredCityIsValid
+                })
+            )
+        }
     }
 
     return (
         <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
                 <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name' />
+                <input 
+                    ref={userNameInputRef}
+                    type='text' 
+                    id='name' />
+                {!inputsIsValid.name && <p>Enter with a valid Name</p>}
             </div>
             <div className={classes.control}>
                 <label htmlFor='street'>Street</label>
-                <input type='text' id='street' />
+                <input 
+                    ref={streetInputRef}
+                    type='text' 
+                    id='street' />
+                {!inputsIsValid.street && <p>Enter with a valid Street Name</p>}
+                
             </div>
             <div className={classes.control}>
                 <label htmlFor='postal'>Postal Code</label>
-                <input type='text' id='postal' />
+                <input 
+                    ref={postalCodeInputRef}
+                    type='text' 
+                    id='postal' />
+                {!inputsIsValid.postalCode && <p>The postal code must have 5 characters</p>}
             </div>
             <div className={classes.control}>
                 <label htmlFor='city'>City</label>
-                <input type='text' id='city' />
+                <input 
+                    ref={cityInputRef}
+                    type='text' 
+                    id='city' />
+                {!inputsIsValid.city && <p>Enter with a valid City Name</p>}
+
             </div>
             <div className={classes.actions}>
                 <button type='button' onClick={props.onCancel}>
